c784ab6e5e64e89f76383a5fdaf9b36e6d6e11d5
smsh119 <suzathoq16@gmail.com>
2024-09-15 00:46:04 +0600

    added hourTaskBoard markup and styles

--- src/components/routinePage/HourTaskBoard.jsx
+++ src/components/routinePage/HourTaskBoard.jsx
@@ -1,9 +1,42 @@
+import styles from "./hourTaskBoard.module.css";
+
 const HourTaskBoard = () => {
   return (
-    <div>
-      <p>task</p>
-      <p>task</p>
-      <p>task</p>
+    <div className={styles.taskBoardContainer}>
+      <h2 className={styles.taskBoardHeader}>Tasks</h2>
+      <h3 className={styles.timeRange}>09:00 to 10:00</h3>
+      <div className={styles.tasks}>
+        {/* Item */}
+        <div className={styles.task}>
+          <p>task description</p>
+          <button>delete</button>
+        </div>
+        {/* Item */}
+        <div className={styles.task}>
+          <p>task description</p>
+          <button>delete</button>
+        </div>
+        {/* Item */}
+        <div className={styles.task}>
+          <p>task description</p>
+          <button>delete</button>
+        </div>
+
+        {/* task input */}
+        <div className={`${styles.task} ${styles.taskInputDiv}`}>
+          <input
+            type="text"
+            placeholder="Task Description"
+            className={styles.taskInput}
+          />
+          <button>Save</button>
+        </div>
+
+        {/* add task div */}
+        <div className={styles.addTaskDiv}>
+          <span>+</span> Add Task
+        </div>
+      </div>
     </div>
   );
 };
