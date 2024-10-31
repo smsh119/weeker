5b64748c244b378d8774a1ab5b01c297732f29f7
smsh119 <suzathoq16@gmail.com>
2024-10-31 20:41:23 +0600

    BE: added cors configuration

--- Back-end/index.js
+++ Back-end/index.js
@@ -13,7 +13,12 @@ const authenticate = require("./middlewares/authenticate.js");
 
 // Middlewares
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 dotenv.config();
