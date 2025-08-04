ad2a22eddcd06ebec3b2a698cadeb21a0ceb7642
smsh119 <suzathoq16@gmail.com>
2025-08-05 00:49:52 +0600

    FE: installed framer-motion, added animation in HourTaskBoard

--- Front-end/src/components/routinePage/HourTaskBoard.jsx
+++ Front-end/src/components/routinePage/HourTaskBoard.jsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { AddTaskSchema } from "../../services/formValidation";
@@ -6,7 +7,6 @@ import styles from "./hourTaskBoard.module.css";
 
 const HourTaskBoard = ({ tasks, time, day, onDelete, onAddTask }) => {
   const [showTaskInput, setShowTaskInput] = useState(false);
-
   const {
     register,
     handleSubmit,
@@ -36,66 +36,86 @@ const HourTaskBoard = ({ tasks, time, day, onDelete, onAddTask }) => {
       <h3 className={styles.timeRange}>{time}</h3>
       <div className={styles.tasks}>
         {/* Items */}
-        {tasks?.length > 0 ? (
-          tasks?.map((task, indx) => (
-            <div
-              className={styles.task}
-              key={indx}
-              style={{ backgroundColor: task.color }}
-            >
-              <p>{task?.description}</p>
-              <button onClick={() => handleDelete(task._id)}>delete</button>
+        <AnimatePresence>
+          {tasks?.length > 0 ? (
+            tasks?.map((task, indx) => (
+              <motion.div
+                className={styles.task}
+                key={task?._id || indx}
+                style={{ backgroundColor: task.color }}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 20 }}
+                transition={{ duration: 0.5 }}
+              >
+                <p>{task?.description}</p>
+                <button onClick={() => handleDelete(task._id)}>delete</button>
+              </motion.div>
+            ))
+          ) : (
+            <div>
+              <p className={styles.noTask}>Time to add a new task!</p>
             </div>
-          ))
-        ) : (
-          <div>
-            <p className={styles.noTask}>Time to add a new task!</p>
-          </div>
-        )}
+          )}
+        </AnimatePresence>
 
         {/* task input */}
-        {showTaskInput && (
-          <form
-            className={`${styles.task} ${styles.taskInputDiv}`}
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <input
-              {...register("taskDescription")}
-              type="text"
-              name="taskDescription"
-              placeholder={
-                errors?.taskDescription
-                  ? errors?.taskDescription?.message
-                  : "Task Description"
-              }
-              className={`${styles.taskInput} ${
-                errors?.taskDescription ? "inputErrorBorder" : ""
-              }`}
-            />
-            <div className={styles.inputButtons}>
-              <input {...register("taskColor")} type="color" name="taskColor" />
-              <button>Save</button>
-              <button
-                onClick={() => {
-                  reset();
-                  setShowTaskInput(false);
-                }}
-              >
-                X
-              </button>
-            </div>
-          </form>
-        )}
+        <AnimatePresence>
+          {showTaskInput && (
+            <motion.form
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              className={`${styles.task} ${styles.taskInputDiv}`}
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <input
+                {...register("taskDescription")}
+                type="text"
+                name="taskDescription"
+                placeholder={
+                  errors?.taskDescription
+                    ? errors?.taskDescription?.message
+                    : "Task Description"
+                }
+                className={`${styles.taskInput} ${
+                  errors?.taskDescription ? "inputErrorBorder" : ""
+                }`}
+              />
+              <div className={styles.inputButtons}>
+                <input
+                  {...register("taskColor")}
+                  type="color"
+                  name="taskColor"
+                />
+                <button>Save</button>
+                <button
+                  onClick={() => {
+                    reset();
+                    setShowTaskInput(false);
+                  }}
+                >
+                  X
+                </button>
+              </div>
+            </motion.form>
+          )}
 
-        {/* add task div */}
-        {!showTaskInput && (
-          <button
-            className={styles.addTaskDiv}
-            onClick={() => setShowTaskInput(true)}
-          >
-            <span>+</span> Add Task
-          </button>
-        )}
+          {/* add task div */}
+          {!showTaskInput && (
+            <motion.button
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              className={styles.addTaskDiv}
+              onClick={() => setShowTaskInput(true)}
+            >
+              <span>+</span> Add Task
+            </motion.button>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
