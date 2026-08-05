2a56852855cd8635702a0ba5ed3ee61a4c817fd1
smsh <suzathoq16@gmail.com>
2026-08-05 23:21:01 +0600

    BE: Fixed bug on addTask to return the latest added task

--- Back-end/controllers/taskController.js
+++ Back-end/controllers/taskController.js
@@ -21,7 +21,7 @@ const addTask = async (req, res) => {
       data.time
     ];
     savedTasksAtTime.sort((a, b) =>
-      a.createdAt.toString().localeCompare(b.createdAt.toString())
+      b.createdAt.toString().localeCompare(a.createdAt.toString())
     );
     res.status(201).json(savedTasksAtTime[0]);
   } catch (err) {
